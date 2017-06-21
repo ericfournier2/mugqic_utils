35ff9ba128481a3e4b81168f103350c21778a269
Eric Fournier <ericfournier2@yahoo.ca>
2017-06-21 14:51:57 -0400

    Add script to clean CHiP mugqic output.

---
+++ clean_mugqic_chip.sh
@@ -0,0 +1,43 @@
+BEDGRAPH=REMOVE
+HOMERPREPARSED=REMOVE
+
+# Read command line arguments
+while [[ $# -gt 1 ]]
+do
+key="$1"
+
+case $key in
+    -b|--keepbedgraph)
+    BEDGRAPH=KEEP
+    ;;
+    -h|--keephomerpreparsed)
+    HOMERPREPARSED=KEEP
+    ;;
+esac
+shift # past argument or value
+done
+
+# Delete intermediaries with no intrinsic value
+rm -rf trim
+
+# Delete redundant alignments.
+# By default only mdup.hardclip.bam is kept.
+for sample in `ls -d alignment/*/ 2>/dev/null` 
+do
+    # List all readsets and remove them.
+    for readset in `ls -d $sample/*/ 2>/dev/null`
+    do
+        rm -rf $readset
+    done
+done
+
+if [ "$BEDGRAPH" = "REMOVE" ]
+then 
+    rm -f tracks/*/*.bedGraph.gz
+    rm -f report/tracks.zip
+fi
+
+if [ "$HOMERPREPARSED" = "REMOVE" ]
+then 
+    rm -rf report/annotation/*/*/preparsed
+fi
